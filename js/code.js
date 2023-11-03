d01ce83e54fe554d96ee4e1b9ff1bb1542201b69
Your Name <aileen1125@gmail.com>
2023-11-03 13:20:33 -0700

    centered logo; dropdown menu working

---
+++ js/code.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+	
+			$(".navigation .navbar .logo-toggle-container .toggle-box").click(function(){
+				$(".navigation .menu").stop().slideToggle();
+			});
+		});
+
+		const toggleBox = document.querySelector(".navigation .navbar .logo-toggle-container .toggle-box");
+
